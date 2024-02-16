5bd72c8da39c45b113886d7783a883e94e5e5f8c
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-16 17:46:39 +0100

    apply bowser props as class

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MasterWord from './game/MasterWord.tsx';
 import './index.css';
+import { applyBowserClass } from './utils/bowser.ts';
+
+applyBowserClass(document.getElementsByTagName('html')[0]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
