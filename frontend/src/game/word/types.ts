1585a8261f86d08e96bd4635874614a9fdc636ca
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-17 19:19:49 +0100

    Update word remove validation from component

---
+++ frontend/src/game/word/types.ts
@@ -0,0 +1,26 @@
+import { TValidationChar } from '../types';
+
+export interface IProps {
+  // row identifier
+  id: string;
+
+  // is the word active - user enters word
+  active?: boolean;
+
+  // current word length
+  wordLength: number;
+
+  // when not active this holds the word to display (previous guess)
+  word?: string;
+
+  // when not mobile this provides user guessed word to be validated
+  commit: (word: string) => void;
+
+  className?: string;
+
+  // is it a mobile and this component is static
+  mobile?: boolean;
+
+  // guessed word validation
+  validated?: TValidationChar[];
+}
