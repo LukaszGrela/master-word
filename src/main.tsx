be97f48fc1884fa51ee5d9249aaa779b6135b02b
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-17 16:01:41 +0100

    production build w/o dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import MasterWord from './game/MasterWord.tsx';
 import './index.css';
 import { applyBowserClass } from './utils/bowser.ts';
+import { disableReactDevTools } from './utils/disableReactDevTools.ts';
 
 applyBowserClass(document.getElementsByTagName('html')[0]);
+
+if (import.meta.env.PROD) {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
