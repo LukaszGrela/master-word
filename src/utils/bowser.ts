8a6655b3c9d32e6afbfaa54f8e6c940c0114cf9b
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-17 15:15:36 +0100

    assure classList friendly

--- src/utils/bowser.ts
+++ src/utils/bowser.ts
@@ -5,27 +5,33 @@ export const getBowserDetails = (): Bowser.Parser.ParsedResult => {
   return parser.getResult();
 };
 
+const makeClassFriendly = (input: string): string => {
+  return input.replace(/\s/g, '_');
+};
+
 export const applyBowserClass = (html: HTMLHtmlElement): void => {
   const result = getBowserDetails();
 
   if (result.os.name) {
-    html.classList.add(result.os.name);
+    html.classList.add(makeClassFriendly(result.os.name));
     if (result.os.version) {
-      html.classList.add(`${result.os.name}-${result.os.version}`);
+      html.classList.add(
+        makeClassFriendly(`${result.os.name}-${result.os.version}`)
+      );
     }
   }
 
   if (result.browser.name) {
-    html.classList.add(result.browser.name);
+    html.classList.add(makeClassFriendly(result.browser.name));
   }
 
   if (result.platform.type) {
-    html.classList.add(result.platform.type);
+    html.classList.add(makeClassFriendly(result.platform.type));
   }
   if (result.platform.vendor) {
-    html.classList.add(result.platform.vendor);
+    html.classList.add(makeClassFriendly(result.platform.vendor));
   }
   if (result.platform.model) {
-    html.classList.add(result.platform.model);
+    html.classList.add(makeClassFriendly(result.platform.model));
   }
 };
