72dde1d301efac587ea8f355c4db933c7f4d53ab
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-18 03:05:50 +0100

    use cors in express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import router from './router';
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,9 @@ const PORT = process.env.PORT || 3001;
 const masterWordApp = express();
 
 masterWordApp.use(express.json());
+
+masterWordApp.use(cors({}));
+
 masterWordApp.use(
   express.urlencoded({
     extended: true,
