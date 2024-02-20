ae2a36d8bcbfee09c51a669fe717ec967eadaacc
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-20 12:11:59 +0100

    fix hasOwn

--- frontend/src/utils/object/hasOwn.ts
+++ frontend/src/utils/object/hasOwn.ts
@@ -1,4 +1,4 @@
 export const hasOwn = <K extends string | number | symbol>(
   o: unknown,
   k: K
-): boolean => Object.prototype.hasOwnProperty.call(o, k);
+): boolean => Object.prototype.hasOwnProperty.call(o || {}, k);
