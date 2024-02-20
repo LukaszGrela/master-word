f64da0ca4b7c7b6beb2a39cedd1d7158e0fdc4b6
Łukasz Grela <lukasz.grela@gmail.com>
2024-02-20 12:14:24 +0100

    load translation file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,23 @@ import MasterWord from './game/MasterWord.tsx';
 import './index.css';
 import { applyBowserClass } from './utils/bowser.ts';
 import { disableReactDevTools } from './utils/disableReactDevTools.ts';
+import { loadTranslation } from './i18n/index.ts';
+import { noop } from './utils/noop.ts';
 
 applyBowserClass(document.getElementsByTagName('html')[0]);
 
 if (import.meta.env.PROD) {
   disableReactDevTools();
 }
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <MasterWord attempts={8} />
-  </React.StrictMode>
-);
+// load default
+loadTranslation('pl')
+  .then(noop)
+  .catch(noop)
+  .finally(() => {
+    // ready or not here I come
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <MasterWord attempts={8} />
+      </React.StrictMode>
+    );
+  });
