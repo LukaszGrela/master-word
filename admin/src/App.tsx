15afb5a995a7ef701e10138fd58e50a0422741b2
Łukasz Grela <lukasz.grela@gmail.com>
2024-03-10 14:59:26 +0100

    ADM: Setup apps providers, routes.

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
+import { CssBaseline } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { Router } from './routes/Router';
+import { SnackbarProvider } from 'notistack';
 
 const App: FC = () => {
-  return <div className='App'>Master Word Admin</div>;
+  return (
+    <StyledEngineProvider injectFirst>
+      <SnackbarProvider maxSnack={5}>
+        <div className='App'>
+          <CssBaseline />
+          <Router />
+        </div>
+      </SnackbarProvider>
+    </StyledEngineProvider>
+  );
 };
 
 export default App;
