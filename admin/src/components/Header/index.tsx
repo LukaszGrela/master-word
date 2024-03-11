69fdb2cea37c21fcf58d817ea17aee305664bf83
Lukasz Grela <lukasz.grela@gmail.com>
2024-03-11 10:14:42 +0100

    Update admin/src/components/Header/index.tsx

--- admin/src/components/Header/index.tsx
+++ admin/src/components/Header/index.tsx
@@ -1,5 +1,4 @@
-import { FC } from 'react';
-import * as React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
