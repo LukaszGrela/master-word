a52bfe65182a839a36575a81dea5b38dc4085daa
Łukasz Grela <lukasz.grela@gmail.com>
2024-03-12 14:56:30 +0100

    configure eslint on backend

---
+++ apps/backend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'src/test/**/*',
+    'src/**/*.spec.ts',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
