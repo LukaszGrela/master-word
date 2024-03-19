8cf5ad77c3dd17b057c31f4439b0b8a90c33dc8c
Łukasz Grela <lukasz.grela@gmail.com>
2024-03-19 16:17:39 +0100

    Add to Header menu items list

--- apps/admin/src/components/Header/types.ts
+++ apps/admin/src/components/Header/types.ts
@@ -1,3 +1,15 @@
+import { TOptionData } from '@repo/common-types';
+import { ReactNode } from 'react';
+import { EPaths } from '../../routes/enums/paths';
+
+export const MENU_SEPARATOR_ITEM = 'SEPARATOR' as const;
+export interface IMenuItems extends TOptionData {
+  icon?: ReactNode;
+  link?: EPaths;
+}
+
 export interface IProps {
   title?: string;
+
+  menu?: IMenuItems[];
 }
