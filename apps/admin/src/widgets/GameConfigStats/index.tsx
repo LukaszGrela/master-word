00d274a7ff00c6d125a9807e10ba64d81dd1e522
Łukasz Grela <lukasz.grela@gmail.com>
2024-03-25 11:56:53 +0100

    Adjust layout of widgets, add configuration link to game config widget

--- apps/admin/src/widgets/GameConfigStats/index.tsx
+++ apps/admin/src/widgets/GameConfigStats/index.tsx
@@ -1,38 +1,73 @@
-import { FC } from 'react';
+import { FC, useCallback, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
+import CardActions from '@mui/material/CardActions';
 import LinearProgress from '@mui/material/LinearProgress';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import { TConfigEntryKey } from '@repo/backend-types/db';
 import { IDictionary } from '@repo/common-types';
 
 import { useGetConfigurationQuery } from '../../store/slices/api';
+import { EPaths } from '../../routes/enums/paths';
+import { FlexGrow } from '../../components/FlexGrow';
+
+const labels: Partial<Record<TConfigEntryKey, string>> = {
+  enabledLanguages: 'Enabled languages',
+  enabledAttempts: 'Enabled attempts',
+  enabledLength: 'Enabled word length',
+};
 
 const GameConfigStats: FC = () => {
+  const navigate = useNavigate();
   const { data, isLoading } = useGetConfigurationQuery('frontend');
 
   type TListEntry = { label: string; value: string };
-  const list: IDictionary<TListEntry> | undefined = data?.reduce(
-    (
-      acc: IDictionary<TListEntry>,
-      { key, value, admin },
-    ): IDictionary<TListEntry> => {
-      if (
-        key === 'enabledAttempts' ||
-        key === 'enabledLanguages' ||
-        key === 'enabledLength'
-      ) {
-        return { ...acc, [key]: { value, label: admin?.label || key } };
-      }
-      return acc;
-    },
-    {},
+  const list: IDictionary<TListEntry> | undefined = useMemo(
+    () =>
+      data?.reduce(
+        (
+          acc: IDictionary<TListEntry>,
+          { key, value },
+        ): IDictionary<TListEntry> => {
+          if (
+            key === 'enabledAttempts' ||
+            key === 'enabledLanguages' ||
+            key === 'enabledLength'
+          ) {
+            return {
+              ...acc,
+              [key]: {
+                value: JSON.stringify(value),
+                label: labels[key] || key,
+              },
+            };
+          }
+          return acc;
+        },
+        {},
+      ),
+    [data],
   );
 
+  const handleReview = useCallback(() => {
+    navigate(EPaths.CONFIG);
+  }, [navigate]);
+
   return (
-    <Card className="widget game-config">
+    <Card
+      className="widget game-config"
+      elevation={3}
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <CardHeader title="Game Config">
         {isLoading && <LinearProgress />}
       </CardHeader>
@@ -70,6 +105,12 @@ const GameConfigStats: FC = () => {
           )}
         </Stack>
       </CardContent>
+      <FlexGrow />
+      <CardActions>
+        <Button size="small" onClick={handleReview}>
+          Config
+        </Button>
+      </CardActions>
     </Card>
   );
 };
