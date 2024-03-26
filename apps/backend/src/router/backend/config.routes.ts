839953a102a89fbf0626ad3ec558e444f7bc5218
Łukasz Grela <lukasz.grela@gmail.com>
2024-03-26 13:19:36 +0100

    save config endpoint

--- apps/backend/src/router/backend/config.routes.ts
+++ apps/backend/src/router/backend/config.routes.ts
@@ -1,11 +1,16 @@
 import { Router, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { getConfiguration, setConfigDefaults } from '../../db/crud/Config.crud';
+import {
+  getConfiguration,
+  setConfigDefaults,
+  setConfigValue,
+} from '../../db/crud/Config.crud';
 import { ensureLoggedIn } from '../helpers';
 import {
   dictionaryDevConnection,
   ensureDictionaryDevConnection,
 } from './dictionary/helpers';
+import { IConfigEntry } from '@repo/backend-types/db';
 
 const router = Router();
 
@@ -41,6 +46,35 @@ router.get(
       const connection = dictionaryDevConnection();
       const config = await setConfigDefaults(connection);
       res.status(StatusCodes.OK).json(config);
+    } catch (error) {
+      console.log(error);
+      res.status(StatusCodes.BAD_REQUEST).json(error);
+    }
+  },
+);
+
+router.post(
+  '/configuration/set/:configKey',
+
+  ensureDictionaryDevConnection(),
+  ensureLoggedIn(),
+
+  async (req: Request, res: Response) => {
+    const { configKey } = req.params;
+
+    const { appId, key, value } = req.body as IConfigEntry;
+
+    try {
+      if (configKey !== key) {
+        throw new Error(
+          `Payload mismatch, declared config key is incorrect. ${configKey}!=${key}`,
+        );
+      }
+
+      const connection = dictionaryDevConnection();
+      const result = await setConfigValue(key, value, appId, connection);
+
+      res.status(StatusCodes.OK).json(result);
     } catch (error) {
       console.log(error);
       res.status(StatusCodes.BAD_REQUEST).json(error);
