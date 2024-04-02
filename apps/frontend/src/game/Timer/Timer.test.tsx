5573f27d6a9e94e45ba4f7322b262c6e0139f6d2
Łukasz Grela <lukasz.grela@gmail.com>
2024-04-03 00:22:43 +0200

    Timer snapshot test

---
+++ apps/frontend/src/game/Timer/Timer.test.tsx
@@ -0,0 +1,68 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { render } from '@testing-library/react';
+
+import Timer from './Timer';
+import { act } from 'react-dom/test-utils';
+
+describe('game', () => {
+  describe('Timer', () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+      vi.setSystemTime(new Date(1979, 5, 13));
+    });
+    afterEach(() => {
+      vi.useRealTimers();
+    });
+    it('Renders with no props', () => {
+      const { container } = render(<Timer />);
+
+      act(() => {
+        vi.advanceTimersByTime(3000);
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+    it('Renders with preset time - now', () => {
+      const { container } = render(<Timer startMs={new Date().getTime()} />);
+
+      act(() => {
+        vi.advanceTimersByTime(10);
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+    it('Renders with preset time - 7sec', () => {
+      const { container } = render(
+        <Timer startMs={new Date().getTime() - 3000} />,
+      );
+
+      act(() => {
+        vi.advanceTimersByTime(4000);
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+    it('Renders with preset time - over a minute', () => {
+      const { container } = render(
+        <Timer startMs={new Date().getTime() - 67000} />,
+      );
+
+      act(() => {
+        vi.advanceTimersByTime(4000);
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+    it('Renders with preset time - over an hour', () => {
+      const { container } = render(
+        <Timer startMs={new Date().getTime() - 3607000} />,
+      );
+
+      act(() => {
+        vi.advanceTimersByTime(4000);
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+  });
+});
--- apps/frontend/src/game/Timer/index.ts
+++ apps/frontend/src/game/Timer/index.ts
@@ -1,1 +1,3 @@
+/* v8 ignore start */
 export { default as Timer } from './Timer';
+/* v8 ignore stop */
