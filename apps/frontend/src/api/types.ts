1145a5d7dabf6b2322cbaa61050d1fc9d098ad95
Łukasz Grela <lukasz.grela@gmail.com>
2024-04-09 22:48:01 +0200

    Add details field toErrorResponse helper

--- apps/frontend/src/api/types.ts
+++ apps/frontend/src/api/types.ts
@@ -8,8 +8,24 @@ export type TPartialGameState = PartialPick<TGameStep, 'word'>[];
 export type TErrorResponse = {
   error: string;
   code: number /* ErrorCodes */;
+
+  details?: string;
 };
 
 export const guardTErrorResponse = (test: unknown): test is TErrorResponse => {
   return !!test && hasOwn(test, 'error') && hasOwn(test, 'code');
+};
+
+export const toErrorResponse = (
+  error: Error | TErrorResponse,
+): TErrorResponse => {
+  if (guardTErrorResponse(error)) {
+    return error;
+  }
+
+  return {
+    code: -1,
+    error: error.message,
+    details: error.name,
+  };
 };
